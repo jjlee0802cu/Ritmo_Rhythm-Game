86d631e67599da4a80b1e505028c9a5f154bd4aa
Justin Lee <jjlee0802@gmail.com>
2018-04-23 15:22:44 -0500

    makes frame
    make frame

---
+++ Frame.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+public class Frame
+{
+    public static void main(String s[]) {
+        JFrame frame = new JFrame("JFrame Example");
+
+        frame.setSize(1024, 768);
+        frame.setVisible(true);
+
+    }
+}
