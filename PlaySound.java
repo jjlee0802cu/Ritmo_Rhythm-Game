2c26fc26ff89e945f10ba8ff32fe53b68c18c0f6
Mally <mtzuoo@gmail.com>
2018-04-24 15:11:14 -0500

    experiments with sound
    sound test now not future very modern

---
+++ PlaySound.java
@@ -0,0 +1,13 @@
+
+public class PlaySound
+{
+
+    public static void main(String[] args)
+    {
+        AePlayWave ae =  new  AePlayWave("YMCA oriental remix.wav");
+        ae.run();
+
+    }
+
+    
+}
