47b52d0a2d85862fdd1f38c91fe348b527c439de
Justin Lee <jjlee0802@gmail.com>
2018-04-25 15:10:44 -0500

    makes color fill
    makes color fill

--- Component.java
+++ Component.java
@@ -7,25 +7,50 @@ public class Component extends JComponent
     static int rectangleX;
     static int lane;
     static boolean fill;
+    static boolean q;
+    static boolean w;
+    static boolean e;
+    static boolean r;
     public static void main(String s[]) {
         JFrame f = new JFrame();
         Component component = new Component();
 
         f.addKeyListener(new KeyAdapter()
             {
-                public void keyPressed(KeyEvent e)
+                public void keyPressed(KeyEvent ke)
                 {
-                    if(e.getKeyCode() == KeyEvent.VK_UP)
+                    if(ke.getKeyCode() == KeyEvent.VK_UP)
                     {
                         fill = true;
+                    }
+                    if(ke.getKeyCode() == KeyEvent.VK_DOWN)
+                    {
+                        fill = false;
+                    }
+                    if(ke.getKeyCode() == KeyEvent.VK_Q)
+                    {
+                        q = true;
+                    }
+                    if(ke.getKeyCode() == KeyEvent.VK_W)
+                    {
+                        w = true;
+                    }
+                    if(ke.getKeyCode() == KeyEvent.VK_E)
+                    {
+                        e = true;
+                    }
+                    if(ke.getKeyCode() == KeyEvent.VK_R)
+                    {
+                        r = true;
                     }
                 }
             });
         f.add(component);
         f.setSize(1024, 768);
         f.setVisible(true);
-
-        int[] lanes = {1,2,3,4,1,2,3,4};
+        
+        
+        int[] lanes = {1,2,4,3,1,4,1,2,1,3,2,4};
         for(int i:lanes){
             rectangleX=i*100;
             while(true && rectangleY<=768-100){
@@ -48,6 +73,19 @@ public class Component extends JComponent
         g.setColor(Color.RED);
         g.drawRect(rectangleX,rectangleY,100,50);
         if(fill){g.fillRect(rectangleX,rectangleY,100,50);
+        }
+        
+        if(q){
+            g.fillRect(100,768-100,100,50);
+        }
+        if(w){
+            g.fillRect(200,768-100,100,50);
+        }
+        if(e){
+            g.fillRect(300,768-100,100,50);
+        }
+        if(r){
+            g.fillRect(400,768-100,100,50);
         }
     }
 }
