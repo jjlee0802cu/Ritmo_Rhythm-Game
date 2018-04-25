c6ea4963f017376043717cb8cc5b8a8153459b49
Justin Lee <jjlee0802@gmail.com>
2018-04-25 14:58:37 -0500

    makes falling blocks with keypress
    makes falling blocks with keypress

--- Component.java
+++ Component.java
@@ -3,41 +3,51 @@ import java.awt.event.*;
 import javax.swing.*;
 public class Component extends JComponent
 {
+    static int rectangleY;
     static int rectangleX;
+    static int lane;
+    static boolean fill;
     public static void main(String s[]) {
         JFrame f = new JFrame();
         Component component = new Component();
 
-        // f.addKeyListener(new KeyAdapter()
-        // {
-            // public void keyPressed(KeyEvent e)
-            // {
-                // if(e.getKeyCode() == KeyEvent.VK_UP)
-                // {
-                    
-                // }
-            // }
-        // });
+        f.addKeyListener(new KeyAdapter()
+            {
+                public void keyPressed(KeyEvent e)
+                {
+                    if(e.getKeyCode() == KeyEvent.VK_UP)
+                    {
+                        fill = true;
+                    }
+                }
+            });
         f.add(component);
         f.setSize(1024, 768);
         f.setVisible(true);
-        
-        while(true){
-            try{
-                Thread.sleep(10);
+
+        int[] lanes = {1,2,3,4,1,2,3,4};
+        for(int i:lanes){
+            rectangleX=i*100;
+            while(true && rectangleY<=768-100){
+                try{
+                    Thread.sleep(1);
+                }
+                catch(Exception e){
+
+                }
+                f.repaint();
+                rectangleY++;
+
             }
-            catch(Exception e){
-                
-            }
-            f.repaint();
-            rectangleX++;
+            rectangleY=0;
         }
+
     }
 
     public void paintComponent(Graphics g){
         g.setColor(Color.RED);
-        g.drawRect(100,100,50,200);
-        
+        g.drawRect(rectangleX,rectangleY,100,50);
+        if(fill){g.fillRect(rectangleX,rectangleY,100,50);
+        }
     }
-
 }
