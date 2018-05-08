e4e14b121977ab9131f0973c1272cdece1e91f68
Daniel Tao <xtao8>
2018-05-08 15:17:54 -0500

    Begin implementation of Timer and Level class

---
+++ TimerThread.java
@@ -0,0 +1,18 @@
+
+/**
+ * TimerThread.java  
+ *
+ * @author:
+ * Assignment #:
+ * 
+ * Brief Program Description:
+ * 
+ *
+ */
+public class TimerThread extends Thread
+{
+    long startTime = System.currentTimeMillis();
+    public void run(){
+        
+    }
+}
